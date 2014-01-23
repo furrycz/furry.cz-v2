5bb5692540099b8d9506259bf5165b57ce6163a9
Natsu13 <kubat130@gmail.com>
2014-01-23 23:17:22 +0100

    [ADD] Furraffinity linker to tinimce

--- app/presenters/AjaxPresenter.php
+++ app/presenters/AjaxPresenter.php
@@ -42,7 +42,44 @@ class AjaxPresenter extends BasePresenter
     }
 	
 	public function renderFurrafinity(){
-		
+		$template = $this->presenter->template;
+		$template->setFile(__DIR__ . '/../templates/components/furrafinitylinker.latte');		
+		//$template->render();
+	}
+	
+	public function renderFurrafinityget($url){
+		$page = file_get_contents($url);
+		$dd = preg_match('/\<u\>(.*)\<\/u\> has elected to make their content available to registered users only\./U',  $page, $pam);
+		if(count($pam)>0){
+			$data = array("Error" => "<b>".$pam[1]."</b> nastavil tento obrázek jako soukromý!");
+		}else{
+			$dd = preg_match('/full_url  \= \"\/\/(.*)\"\;/U',  $page, $mam);
+			$img = explode("/",$mam[1]);
+			$img = end($img);
+	
+			$fields = array(
+						'URL' => urlencode("http://".$mam[1])
+					);
+			$fields_string="";			
+			foreach($fields as $key=>$value) { $fields_string .= $key.'='.$value.'&'; }
+			rtrim($fields_string, '&');			
+	
+			$cSession = curl_init(); 
+			curl_setopt($cSession, CURLOPT_URL,"http://www.katedrala.cz/anonym/nph-agent.cgi/011110A/k-cebkl:/=2ffgneg");
+			curl_setopt($cSession, CURLOPT_POST, count($fields));
+			curl_setopt($cSession, CURLOPT_POSTFIELDS, $fields_string);
+			curl_setopt($cSession, CURLOPT_RETURNTRANSFER,true);
+			curl_setopt($cSession, CURLOPT_HEADER, true); 
+			$result=curl_exec($cSession);
+			$last_url = curl_getinfo($cSession, CURLINFO_EFFECTIVE_URL);
+			curl_close($cSession);	
+			$result = explode(":",$result);
+			$res = $result[4];$i=5;
+			while(isset($result[$i])){$res.=":".$result[$i];$i++;}	
+			$res=trim($res);
+			$data = array("Error" => 0, "urlImage" => $res, "urlDefault" => $url);
+		}
+		$this->sendResponse(new JsonResponse($data));
 	}
 	
 }	
